68b74bad66b4969d7c1f5630f45916bf714c87f3
chahalvikas2022 <chahalvikash2022@gmail.com>
2026-06-01 14:08:07 +0530

    inital commit

---
+++ _example/public_key/example.tf
@@ -0,0 +1,16 @@
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+####----------------------------------------------------------------------------------
+##  Module      : public KEY PAIR
+####----------------------------------------------------------------------------------
+module "public_keypair" {
+  source = "../../"
+
+  name        = "public-key"
+  environment = "test"
+  label_order = ["name", "environment"]
+  public_key  = ""
+}
